3e988d7c6a99068d1d2694e20c4bdb48200f11da
tracer-ics2016 <tracer@njuics.org>
2021-01-16 19:48:52 -0800

    >  compile NEMU 3018216310 linqing Linux ubuntu 5.0.0-23-generic #24~18.04.1-Ubuntu SMP Mon Jul 29 16:12:28 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:48:52 up 13:50,  1 user,  load average: 0.13, 0.10, 0.11 e8852d207bd785c972b1704c39f207498c24c6b7

--- nemu/src/cpu/exec/data-mov/gdt.c
+++ nemu/src/cpu/exec/data-mov/gdt.c
@@ -1,12 +1,12 @@
 #include "cpu/exec/helper.h"
+make_helper(lgdt)
+{
+    decode_rm_l(eip + 1);
+    uint32_t addr = op_src->addr;
+    cpu.GDTR.limit = swaddr_read(addr, 2);
+     cpu.GDTR.base_l = swaddr_read(addr + 2, 2);
+     cpu.GDTR.base_h = swaddr_read(addr + 4, 2);
 
- make_helper(lgdt)
- {
-     uint16_t *data;
-     data = (uint16_t *)(op_src->val + data - data);
-     cpu.GDTR.limit = data[0];
-     cpu.GDTR.base_l = data[1];
-     cpu.GDTR.base_h = data[2];
-
-     return 5;
+ 
+     return 6;
  } 
